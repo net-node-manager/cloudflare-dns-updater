eeffc6edf228532e8b49d3a9a47d723942a5793c
net-node-manager <hesaart3@gmail.com>
2026-02-26 06:10:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,19 @@ jobs:
     permissions:
       contents: write
     steps:
-      - name: Checkout Repository
-        uses: actions/checkout@v3
-        with:
-          token: ${{ secrets.GITHUB_TOKEN }}
-
+      - uses: actions/checkout@v3
       - name: Set up Python
         uses: actions/setup-python@v4
         with:
           python-version: '3.x'
-
       - name: Install dependencies
         run: pip install requests
-
-      - name: Run Script
+      - name: Run Scanner
         run: python main.py
-
-      - name: Commit and Push Changes
+      - name: Commit and Push
         run: |
-          git config --global user.name "net-node-manager"
-          git config --global user.email "bot@net-node-manager.studio"
+          git config --global user.name 'net-node-manager'
+          git config --global user.email 'bot@net-node-manager.studio'
           git add list.txt
           git commit -m "Automated IP update" || exit 0
           git push
