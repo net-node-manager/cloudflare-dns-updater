0345123c0e18434915f2074f673e8479a3d1eee4
net-node-manager <hesaart3@gmail.com>
2026-02-26 05:51:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ on:
   workflow_dispatch:
 
 jobs:
-  build:
+  run-updater:
     runs-on: ubuntu-latest
     steps:
-      - uses: actions/checkout@v3
+      - name: Checkout Repository
+        uses: actions/checkout@v3
+
       - name: Set up Python
         uses: actions/setup-python@v4
         with:
           python-version: '3.x'
+
       - name: Install dependencies
         run: pip install requests
-      - name: Run Script
+
+      - name: Run IP Scanner
         run: python main.py
-      - name: Commit and Push
+
+      - name: Commit and Push Changes
         run: |
-          git config --global user.name 'net-node-manager'
-          git config --global user.email 'bot@net-node-manager.com'
+          git config --global user.name "net-node-manager"
+          git config --global user.email "bot@net-node-manager.studio"
           git add list.txt
-          git commit -m "Update IP list" || exit 0
+          git commit -m "Automated IP update" || exit 0
           git push
