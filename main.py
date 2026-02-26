bb791bfbb91423751c42c76dfa83d74742b6bac0
net-node-manager <hesaart3@gmail.com>
2026-02-26 07:18:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import requests
 
-def get_ips():
-    url = "https://raw.githubusercontent.com/vfarid/cf-clean-ips/main/list.txt"
+def update_node_list():
+    source = "https://raw.githubusercontent.com/vfarid/cf-clean-ips/main/list.txt"
     try:
-        r = requests.get(url)
-        # Filtering top 20 stable IPs
-        ips = [l.split(' ')[0] for l in r.text.split('\n') if l]
-        return ips[:20]
-    except:
-        return []
+        response = requests.get(source)
+        # Filtering top 20 IPs
+        ips = [line.split(' ')[0] for line in response.text.split('\n') if line.strip()]
+        
+        with open('list.txt', 'w') as f:
+            f.write('\n'.join(ips[:20]))
+        print("SUCCESS: list.txt created.")
+    except Exception as e:
+        print(f"ERROR: {e}")
 
 if __name__ == "__main__":
-    clean_ips = get_ips()
-    if clean_ips:
-        with open('list.txt', 'w') as f:
-            f.write('\n'.join(clean_ips))
-        print("Done: list.txt created.")
+    update_node_list()
